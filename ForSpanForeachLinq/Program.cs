076f2d5e93b7af43f90fdc5438fc0dd7de7618cb
Ahmed Ayman Mansour <ahmadahmad00790@yahoo.com>
2023-06-01 15:27:56 +0300

    Add project files.

---
+++ ForSpanForeachLinq/Program.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Running;
+using ForSpanForeachLinq;
+using System.Diagnostics;
+
+/*
+#region old code
+// Define some basic types for testing each loop type.
+var ints = new int[] { 1, 2, 3, 5, 6 };
+
+Span<int> intSpans = new Span<int>(ints);
+List<int> intList = new List<int>(ints);
+// Normal for loop with array
+var watch = Stopwatch.StartNew();
+for (int i = 0; i < ints.Count(); i++)
+{
+    Console.WriteLine(ints[i]);
+}
+watch.Stop();
+Console.WriteLine($"Elapsed time FOR LOOP with ARRAY = {watch.ElapsedMilliseconds / 1000.0}");
+// Normal for loop with list
+watch.Start();
+for (int i = 0; i < intList.Count; i++)
+{
+    Console.WriteLine(intList[i]);
+}
+watch.Stop();
+Console.WriteLine($"Elapsed time FOR LOOP with LIST = {watch.ElapsedMilliseconds / 1000.0}");
+// Normal for loop with span
+watch.Start();
+for (int i = 0; i < intSpans.Length; i++)
+{
+    Console.WriteLine(intSpans[i]);
+}
+watch.Stop();
+Console.WriteLine($"Elapsed time FOR LOOP with SPAN = {watch.ElapsedMilliseconds / 1000.0}");
+#endregion
+*/
+
+
+BenchmarkRunner.Run<TestingClasses>();
+
---
+++ ForSpanForeachLinq/TestingClasses.cs
@@ -0,0 +1,87 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ForSpanForeachLinq;
+
+namespace ForSpanForeachLinq
+{
+    /// <summary>
+    /// A testing class for normal For loop, For loop with a span, Foreach with array, Foreach with span, Parallel Foreach, Parallel LINQ
+    /// <br>Made by Software Engineer \ Ahmed A. Mansour</br>
+    /// <br>Check my github: <a href="https://www.github.com/xx7Ahmed7xx"></a></br> 
+    /// <br>Note: run inside release mode, by calling BenchmarkRunner.Run&lt;TestingClasses&gt;() in the entry point of your program.</br>
+    /// </summary>
+    [MemoryDiagnoser]
+    public class TestingClasses
+    {
+        public static int[] ints;
+        [Params(100,1000)]
+        public static int Size { get; set; }
+
+        static ReadOnlySpan<int> intSpans => new Span<int>(ints);
+        //static List<int> intList = new List<int>(ints);
+
+        [GlobalSetup]
+        public void SetUp()
+        {
+            ints = new int[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                ints[i] = i;
+            }
+        }
+
+        [Benchmark]
+        public void ForArray()
+        {
+            for (int i = 0; i < ints.Count(); i++)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForSpan()
+        {
+            for (int i = 0; i < intSpans.Length; i++)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForeachArray()
+        {
+            foreach (var item in ints)
+            {
+
+            }
+        }
+
+        [Benchmark]
+        public void ForeachSpan()
+        {
+            foreach (var item in intSpans)
+            {
+                
+            }
+        }
+
+        [Benchmark]
+        public void ParallelForeach()
+        {
+            Parallel.ForEach(ints, (int1) =>
+            {
+
+            });
+        }
+
+        [Benchmark]
+        public void ParallelLINQ()
+        {
+            ints.AsParallel().ForAll((int1) =>
+            {
+
+            });
+        }
+    }
+}
